b560f2a99e48ed7776e158e649e12cfee3e9ef5d
hubo <luobo@chongdonglvxing.com>
2017-04-19 17:37:34 +0800

    h

--- js/js.js
+++ js/js.js
@@ -14,6 +14,30 @@ $(function() {
 	})
 })
 
+function get_uuid() {
+	if(localStorage.getItem('uuid')) {
+		return localStorage.getItem('uuid');
+	} else {
+		var uuid = getuuid();
+		localStorage.setItem('uuid', uuid);
+		return uuid;
+	}
+}
+
+function getuuid() {
+	var s = [];
+	var hexDigits = "0123456789abcdef";
+	for(var i = 0; i < 36; i++) {
+		s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+	}
+	s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+	s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+	s[8] = s[13] = s[18] = s[23] = "-";
+
+	var uuid = s.join("");
+	return uuid;
+}
+
 //手机号码验证
 function checkMobile() {
 	var mobile = $('#mobile').val();
@@ -109,15 +133,24 @@ function validateCode() {
 	var inputCode = document.getElementById("inputCode").value;
 	if(inputCode.length <= 0) {
 		$('#inputCode').siblings('.zc_correct').show().children('img').attr('src', 'images/error.png');
-		$("#muserv1_code").attr("disabled","disabled").css("background","#ccc")
+		$("#muserv1_code").attr("disabled", "disabled").css({
+			"background": "#ccc",
+			"cursor": "no-drop"
+		})
 		return false;
 	} else if(inputCode.toUpperCase() != code.toUpperCase()) {
 		$('#inputCode').siblings('.zc_correct').children('img').attr('src', 'images/error.png');
-		$("#muserv1_code").attr("disabled","disabled").css("background","#ccc")
+		$("#muserv1_code").attr("disabled", "disabled").css({
+			"background": "#ccc",
+			"cursor": "no-drop"
+		})
 		return false;
 	} else {
 		$('#inputCode').siblings('.zc_correct').children('img').attr('src', 'images/correct.png');
-		$("#muserv1_code").removeAttr("disabled").css("background","#eb6100")
+		$("#muserv1_code").removeAttr("disabled").css({
+			"background": "#eb6100",
+			"cursor": "pointer"
+		})
 		return true;
 	}
 }
@@ -125,7 +158,7 @@ function validateCode() {
 function checkCode() {
 	var _this = $(this);
 	var icode = $('#icode').val();
-	if(!(/^\d{6}$/.test(icode))) {
+	if(!(/^\d{4}$/.test(icode))) {
 		$('#icode').siblings('.zc_correct').show();
 		$('#icode').siblings('.zc_correct').children('img').attr('src', 'images/error.png');
 		return false;
@@ -168,7 +201,7 @@ $("#muserv1_code").on("click", function() {
 		type: "POST",
 		success: function(data) {
 			if(data.code == 20000) {
-				$("#muserv1_code").css("background","#ccc")
+				$("#muserv1_code").css("background", "#ccc")
 				showTime();
 			} else {
 				alert("请输入正确手机号")
@@ -177,51 +210,69 @@ $("#muserv1_code").on("click", function() {
 		}
 	})
 })
-$("#myform").on("click", function() {
-		var phonenum = $("#mobile").val();
-		var code = $("#icode").val();
-		$.ajax({
-			url: "https://sms.chongdonglvxing.com/verifyCode",
-			data: {
-				tel: phonenum,
-				verify: code
-			},
-			type: "POST",
-			success: function(data,e) {
-				if(data.code !== 20000|| checkMobile() == false || checkCode() == false || validateCode() == false) {
-					alert("请填写正确信息")
-					e.preventDefault();
-					return false;
 
-				} else {
-					$(".registered_phone").hide();
-					$(".registered_box").show();
-				}
-//				if(data.code == 20000) {
-//					alert("OK");
-//					$(".registered_phone").hide();
-//					$(".registered_box").show();
-//				} else {
-//					e.preventDefault();
-//					return false;
-//					alert("error!")
-//				}
-				console.log(data)
-			}
-		})
-	})
-	//下一步判断信息是否填写正确
-//$(".zc_submit").click(function(e) {
-//	if(checkMobile() == false || checkCode() == false || validateCode() == false) {
-//		alert("请填写正确信息")
-//		e.preventDefault();
-//		return false;
-//
+//function browserRedirect() {
+//	var sUserAgent = navigator.userAgent.toLowerCase();
+//	var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+//	var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+//	var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+//	var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+//	var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+//	var bIsAndroid = sUserAgent.match(/android/i) == "android";
+//	var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+//	var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+//	if(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+//		return "phone";
 //	} else {
-//		$(".registered_phone").hide();
-//		$(".registered_box").show();
+//		return "pc";
 //	}
-//});
+//}
+$("#myform").on("click", function(e) {
+	var phonenum = $("#mobile").val();
+	var code = $("#icode").val();
+	var hb_fun = e;
+	$.ajax({
+		url: "https://api.chongdonglvxing.com/h5/tc/quick",
+		data: {
+			phone: phonenum,
+			verify: code,
+			uuid: get_uuid(),
+			client_system: "Web"
+		},
+		type: "POST",
+		success: function(data) {
+			if(checkMobile() == false || checkCode() == false || validateCode() == false) {
+				alert("请填写完整正确信息")
+				hb_fun.preventDefault();
+				return false;
+			}
+			if(data.code == 20000) {
+				alert("该手机号已注册成功，请下载App直接登录。")
+				hb_fun.preventDefault();
+				return false;
+			}
+			if(data.code == 10015) {
+				$(".registered_phone").hide();
+				$(".registered_box").show();
+			}
+			if(data.code == 10009) {
+				alert("验证码输入错误")
+				hb_fun.preventDefault();
+				return false;
+			}
+			if(data.code == 10025) {
+				alert("用户被驳回")
+			}
+			if(data.code == 10019) {
+				alert("信息正在审核中，请耐心等待。")
+				hb_fun.preventDefault();
+				return false;
+			}
+			console.log(data)
+		}
+	})
+})
+
 $("#submite").on("click", function() {
 	var names = $("#names").val();
 	var sex = $("#sex").val();
@@ -232,34 +283,95 @@ $("#submite").on("click", function() {
 	var language = $("#language").val();
 	var line = $("#line").val();
 	var tcm = $("#tcm").val();
-	var logoUp4 = $("#logoUp4").val();
-	var logoUp5 = $("#logoUp5").val();
-	$.ajax({
-		url: "https://api.chongdonglvxing.com/v1/tc/register",
-		data: {
-			tc_type: tcm,
-			real_name: names,
-			gender: sex,
-			card_num: code,
-			email: email,
-			address: seat,
-			work_year: num,
-			language: language,
-			good_line: line,
-			positive_pic: logoUp4,
-			back_pic: logoUp5
-
-		},
-		type: "POST",
-		success: function(data) {
-			if(data.code == 20000) {
-				window.open("http://www.chongdonglvxing.com");
-			} else {
-				alert("错误")
-			}
-			console.log(data)
+	var logoUp4 = logoUp5 = 1;
+		if(names == '' || sex == '' || code == '' || num == '') {
+			alert("请填写完整信息");
+			return false;
 		}
+		if($("#logoUp4")[0].files.length === 0 || $("#logoUp5")[0].files.length === 0) {
+			alert("请先上传图片");
+			return false;
+		}
+		if(checkSeat() == false || checkNames() == false || checkIdcode() == false || checkNum() == false) {
+			alert("请填写正确信息");
+			return false;
+		}
+	//执行压缩插件
+	//$("#file")[0].files[0]就是要提交的图片文件值
+	lrz($("#logoUp4")[0].files[0]).then(function(rst) {
+		// rst是处理结果
+		// 处理成功会执行
+		logoUp4 = rst.base64;
+	}).catch(function(err) {
+		// 处理失败会执行
+		alert("图片转码失败，请重新上传")
 	})
+	lrz($("#logoUp5")[0].files[0]).then(function(rst) {
+		// rst是处理结果
+		// 处理成功会执行
+		logoUp5 = rst.base64;
+
+	}).catch(function(err) {
+		// 处理失败会执行
+		alert("图片转码失败，请重新上传")
+	})
+	var oPic = function() {
+		$.ajax({
+			url: "https://api.chongdonglvxing.com/h5/tc/upload",
+			data: {
+				positive_pic: logoUp4,
+				back_pic: logoUp5
+			},
+			type: "POST",
+			success: function(data) {
+				if(data.code == 20000) {
+					console.log(data);
+					doFnSendUserInfo(data);
+				} else {
+					alert("信息填写错误")
+				}
+
+			}
+		})
+	}
+	var doFnSendUserInfo = function(data) {
+		$.ajax({
+			url: "https://api.chongdonglvxing.com/h5/tc/register",
+			data: {
+				tc_type: tcm,
+				real_name: names,
+				gender: sex,
+				card_num: code,
+				email: email,
+				address: seat,
+				work_year: num,
+				language: language,
+				good_line: line,
+				positive_pic: data.data.positive_pic,
+				back_pic: data.data.back_pic,
+				uuid: get_uuid(),
+				client_system: "Web"
+			},
+			type: "POST",
+			success: function(data) {
+				if(data.code == 20000) {
+//					console.log(positive_pic)
+					alert("提交成功，我们会尽快审核信息并与您取得联系，请保持电话畅通。");
+					localStorage.clear();
+					location.href="http://www.chongdonglvxing.com"
+				} else {
+					alert("信息填写错误")
+				}
+				console.log(data)
+			}
+		})
+	}
+	var timerToSendUserInfo = setInterval(function() {
+		if(logoUp4 !== 1 && logoUp5 !== 1) {
+			oPic();
+			clearInterval(timerToSendUserInfo);
+		}
+	}, 200)
 })
 $(".zc_tel ul li").click(function() {
 		$(this).addClass("curr").siblings().removeClass("curr")
@@ -314,6 +426,11 @@ $("#sex_cons .sex_con").off("click").on("click", function() {
 			if(hb_arr[i] == name) {
 				hb_arr.splice(i, 1);
 				$("#line").val(hb_arr.join(","));
+				return false;
+			}
+			if($("#sex_cons .curr").length > 3) {
+				alert("我是有上限的，不能再多了哦！");
+				$(this).removeClass("curr");
 				return false;
 			}
 		}
